a7f1e42c379a2a849647b16d16175f5d42f9429f
marcelocataoca <marcelokenjicatao@gmail.com>
2021-08-10 23:46:58 -0300

    add like function button

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type QuestionType = {
     id: string;
@@ -10,6 +11,8 @@ type QuestionType = {
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likeCount: number;
+    likeId: string | undefined;
   };
 
   type FirebaseQuestions = Record<
@@ -22,10 +25,14 @@ type QuestionType = {
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likes: Record<string, {
+      authorId: string | undefined
+    }>
   }
 >;
 
 export function useRoom(roomId: string){
+  const { user } = useAuth();
     const [questions, setQuestions] = useState<QuestionType[]>([]);
     const [title, setTitle] = useState("");
 
@@ -44,12 +51,18 @@ export function useRoom(roomId: string){
                 author: value.author,
                 isHighlighted: value.isHighlighted,
                 isAnswered: value.isAnswered,
+                likeCount: Object.values(value.likes ?? {}).length,
+                likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0],
               };
             }
           );
           setTitle(databaseRoom.title);
           setQuestions(parsedQuestions);
         });
-      }, [roomId]);
+        //remove os listeners declarados dentro do hook
+        return() => {
+          roomRef.off('value');
+        }
+      }, [roomId, user?.id]);
     return {questions, title}
 }
